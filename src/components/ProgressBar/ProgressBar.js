161f9b39b8b129b665b1d23aa9bf62e17d67f6ea
David Glymph <Woozl@users.noreply.github.com>
2023-01-26 20:52:46 -0500

    progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,55 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    height: '8px',
+    borderRadius: '4px',
+  },
+  medium: {
+    height: '12px',
+    borderRadius: '4px',
+  },
+  large: {
+    height: '24px',
+    borderRadius: '8px',
+  }
+}
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  width: var(--value);
+  height: 100%;
+`;
+
+const Track = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  height: 12px;
+  border-radius: 4px;
+  box-shadow: inset 0 2px 4px 0 ${COLORS.transparentGray35};
+  overflow: hidden;
+`
+
+const InnerOverflow = styled.div`
+  height: 16px;
+  overflow: hidden;
+  margin: 4px;
+  border-radius: 4px;
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <>
+      <Track style={SIZES[size]}>
+        {
+          size === 'large'
+            ? <InnerOverflow><Bar aria-valuenow={ value } style={{ '--value': value + '%'}}></Bar></InnerOverflow>
+            : <Bar aria-valuenow={ value } style={{ '--value': value + '%'}}></Bar>
+        }
+      </Track>
+      <VisuallyHidden>{value + '%'}</VisuallyHidden>
+    </>
+    );
 };
 
 export default ProgressBar;
