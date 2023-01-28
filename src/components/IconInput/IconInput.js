ba9245d4f8d1813d428d2657679e4f253276c4a8
David Glymph <Woozl@users.noreply.github.com>
2023-01-28 17:53:24 -0500

    initial iconinput styles

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,33 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--font-size': 14 / 16 + 'rem',
+    '--padding': '4px',
+    '--border-width': '1px',
+  },
+  large: {
+    '--font-size': 18 / 16 + 'rem',
+    '--padding': '8px',
+    '--border-width': '2px',
+  }
+}
+
+const Input = styled.input`
+  font-size: var(--font-size);
+  color: ${COLORS.gray700};
+  border: none;
+  border-bottom: var(--border-width) solid ${COLORS.black};
+  padding: var(--padding);
+  width: ${p => p.width}px;
+  outline-offset: 2px;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +40,12 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return (
+    <>
+      <Input style={SIZES[size]} placeholder={placeholder} width={width} />
+      <VisuallyHidden>{label}</VisuallyHidden>
+    </>
+  );
 };
 
 export default IconInput;
